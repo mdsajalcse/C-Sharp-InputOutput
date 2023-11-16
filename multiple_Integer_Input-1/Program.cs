2f59ea9b3efb3e92006408625af7c3c8371836c0
sajal <sajalcse2018@gmail.com>
2023-11-16 17:25:42 +0600

    input multiple value from user

---
+++ multiple_Integer_Input-1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+var line=Console.ReadLine();
+var data = line.Split(" ");
+var num1 = int.Parse(data[0]);
+var num2 = int.Parse(data[1]);
+var num3 = int.Parse(data[2]);
+var num4 = int.Parse(data[3]);
+
+var sumNumber=num1+ num2 + num3 + num4;
+Console.WriteLine(sumNumber);
