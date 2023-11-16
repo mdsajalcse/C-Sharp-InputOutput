c9a3c68180d12846cbf53644efea402770107024
sajal <sajalcse2018@gmail.com>
2023-11-16 17:02:03 +0600

    how to input integer value

---
+++ multiple_Integer_Input/Program.cs
@@ -0,0 +1,9 @@
+﻿//C# Multiple integer Value input
+
+var line = Console.ReadLine();
+var data = line.Split(' ');
+var i1 = int.Parse(data[0]); //first integer
+var i2 = int.Parse(data[1]); //second integer
+
+int sum = i1 + i2;
+Console.WriteLine(sum);
